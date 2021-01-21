71b983103c2dc4b0c4e411ad35a783b8eb73abb0
Karoline <karoline.knoth@gmail.com>
2021-01-21 11:25:00 +0100

    improved train.py, changes for ICT server

--- train.py
+++ train.py
@@ -81,10 +81,18 @@ def data_transforms():
         ])
         test_transforms = val_transforms
     elif FLAGS.data_transforms == 'cifar10':
-        train_transforms = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        val_transforms = train_transforms
-        test_transforms = train_transforms
+        train_transforms = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
+        
+        test_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        val_transforms = test_transforms
     else:
         try:
             transforms_lib = importlib.import_module(FLAGS.data_transforms)
@@ -135,10 +143,8 @@ def dataset(train_transforms, val_transforms, test_transforms):
             train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=train_transforms)
             test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=train_transforms)
-            val_set = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=train_transforms)
-
+                                       download=True, transform=test_transforms)
+            val_set = test_set
 
     else:
         try:
@@ -199,10 +205,9 @@ def data_loader(train_set, val_set, test_set):
             drop_last=getattr(FLAGS, 'drop_last', False))
         test_loader = val_loader
     elif FLAGS.data_loader == 'cifar10':
-        train_loader = torch.utils.data.DataLoader(train_set, batch_size=4,
-                                         shuffle=True, num_workers=2)
-        val_loader = train_loader
-        test_loader = val_loader
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2)
+        test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+        val_loader = test_loader
     else:
         try:
             data_loader_lib = importlib.import_module(FLAGS.data_loader)
